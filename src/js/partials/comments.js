8544e64a0d48f307d4590445e12c416cc116f65b
peterwilmshurst <peterwilmshurst@hotmail.com>
2020-10-08 15:23:32 +0100

    add date formatting via day.js

--- src/js/partials/comments.js
+++ src/js/partials/comments.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    var url = "https://objective-fermi-26fd48.netlify.app/data/comments.json";
+
+    Handlebars.registerHelper('formatDate', function(property) {
+        const milliseconds = property * 1000 
+        const dateObject = dayjs(milliseconds).format('dddd, D MMMM, YYYY h:mm A')
+        return new Handlebars.SafeString(dateObject);
+    });
+
+    var url = "https://cors-anywhere.herokuapp.com/https://objective-fermi-26fd48.netlify.app/data/comments.json";
     var commentsTemplate = $("#commentsTemplate").html();
     var compiledCommentsTemplate = Handlebars.compile(commentsTemplate);
 
